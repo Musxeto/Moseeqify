660d6f85a09a085b11951ff9c822e20fcd8d0b06
Mus <138971833+Musxeto@users.noreply.github.com>
2024-06-07 14:58:20 +0500

    data file

---
+++ Database/data.sql
@@ -0,0 +1,71 @@
+INSERT INTO Artist (name, bio, profilepiclink)
+VALUES ('Playboi Carti', 'Jordan Terrell Carter, known professionally as Playboi Carti, is an American rapper.', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/carti.jpg?alt=media&token=d1378bc2-85e1-4acd-aa1f-c8257c66e7a9');
+   
+                      
+INSERT INTO Album (name, artistID, releasedate, coverimagelink)
+VALUES ('Whole Lotta Red', 1, '2020-12-25', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/whole%20lotta%20red.png?alt=media&token=907fd3f4-4927-43b4-9b3e-432e90213712');
+
+INSERT INTO Genre (genreName)
+    VALUES ('Hip Hop');
+
+INSERT INTO Song (title, artistID, albumID, genreName, duration, audiolink)
+VALUES 
+('Rockstar Made', 1, 1, 'Hip Hop', '00:03:35', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Rockstar%20Made.mp3?alt=media&token=a0e99151-a865-4c23-adee-c99a58efd76e'),
+('Go2DaMoon', 1, 1, 'Hip Hop', '00:03:03', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Go2DaMoon.mp3?alt=media&token=53e4bff8-7765-4d9e-8649-949de333d534'),
+('Stop Breathing', 1, 1, 'Hip Hop', '00:02:11', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Stop%20Breathing.mp3?alt=media&token=506f35ea-f021-40b3-a970-f3cdd671f1ad'),
+('Beno!', 1, 1, 'Hip Hop', '00:02:32', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Beno!.mp3?alt=media&token=5a6e57e7-1c0b-4eb0-9411-623dfe2fdc0a'),
+('JumpOutTheHouse', 1, 1, 'Hip Hop', '00:01:33', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/JumpOutTheHouse.mp3?alt=media&token=9fceffe4-2170-40cd-b160-fd1cf324d8e3'),
+('M3tamorphosis', 1, 1, 'Hip Hop', '00:03:26', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/M3tamorphosis.mp3?alt=media&token=4bef6321-2da6-435f-a51c-c5c84dd4fe9a'),
+('Slay3r', 1, 1, 'Hip Hop', '00:02:38', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Slay3r.mp3?alt=media&token=6fdb19b9-d66b-4560-929b-84cd34f371e1'),
+('No Sl33p', 1, 1, 'Hip Hop', '00:02:00', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/No%20Sl33p.mp3?alt=media&token=6662906a-00e8-4e31-a090-98d7e94eaa69'),
+('New Tank', 1, 1, 'Hip Hop', '00:03:20', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/New%20Tank.mp3?alt=media&token=6ad77335-8f4d-41d5-91b7-b3a5c5c61b64'),
+('Teen X', 1, 1, 'Hip Hop', '00:02:43', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Teen%20X.mp3?alt=media&token=86512e5e-ccde-4302-b90c-b4df678cf348'),
+('Meh', 1, 1, 'Hip Hop', '00:02:57', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Meh.mp3?alt=media&token=7c5142f3-cbb8-4a55-83a6-ea1c289f9a2c'),
+('Vamp Anthem', 1, 1, 'Hip Hop', '00:02:06', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Vamp%20Anthem.mp3?alt=media&token=e8f128e9-fe5e-4c4d-861b-ae7c6d740051'),
+('New N3on', 1, 1, 'Hip Hop', '00:02:24', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/New%20N3on.mp3?alt=media&token=14304046-5fdc-439b-bf43-3f3495e0e229'),
+('Control', 1, 1, 'Hip Hop', '00:03:17', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Control.mp3?alt=media&token=43ca73fb-0c2c-4124-82d8-1e3fcf49d870'),
+('Punk Monk', 1, 1, 'Hip Hop', '00:03:13', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Punk%20Monk.mp3?alt=media&token=6f8c6a54-70d0-45fb-8083-58b909e5c444'),
+('On That Time', 1, 1, 'Hip Hop', '00:02:53', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/On%20That%20Time.mp3?alt=media&token=aa1d44ac-7ca2-4aef-974c-befaa537445d'),
+('King Vamp', 1, 1, 'Hip Hop', '00:03:06', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/King%20Vamp.mp3?alt=media&token=0d23cd51-4995-4b9b-92f7-db8859bbd452'),
+('Place', 1, 1, 'Hip Hop', '00:03:43', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Place.mp3?alt=media&token=fa980804-862a-489e-80f4-4748b3d1319d'),
+('Sky', 1, 1, 'Hip Hop', '00:02:57', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Sky.mp3?alt=media&token=c7c83856-ebc8-49c8-b135-10cd5d00f2c2'),
+('Over', 1, 1, 'Hip Hop', '00:03:15', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Over.mp3?alt=media&token=20186298-b878-447d-95f0-7b5f4d8e7fc1'),
+('ILoveUIHateU',1, 1, 'Hip Hop', '00:02:47', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/ILoveUIHateU.mp3?alt=media&token=326f81a1-b6f6-4dd9-b77e-7d200f907290'),
+('Die4Guy',1, 1, 'Hip Hop', '00:03:25', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Die4Guy.mp3?alt=media&token=c2e2020c-a62d-4fcd-ab72-50586e153569'),
+('Not PLaying', 1, 1, 'Hip Hop', '00:02:43', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Not%20PLaying.mp3?alt=media&token=fcaa73d0-e6e5-44cc-b57f-204f00a5383c'),
+('F33l Lik3 Dyin',1, 1,'Hip Hop', '00:03:15', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/F33l%20Lik3%20Dyin.mp3?alt=media&token=63658368-e9d8-4daf-b6e7-e67d6ff829ac');
+
+
+INSERT INTO Artist (name, bio, profilepiclink)
+VALUES 
+    ('Lil Mussi W', 'WE UP NIGGA.', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/lil%20mussi%20w.jpg?alt=media&token=08e5afe6-528a-4eb1-a1ab-abdc738cac2b')
+
+
+
+INSERT INTO Genre (genreName)
+VALUES 
+    ('Rap')
+
+	
+
+INSERT INTO Album (name, artistID, releasedate, coverimagelink)
+VALUES 
+    ('PAN DI PHUSSI', 2, '2023-04-30', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Screenshot_2023-04-29-22-57-59-681-edit_com.picsart.studio.jpg?alt=media&token=b7f1e891-76ed-49b0-9213-ffbcde0b5db1')
+
+
+INSERT INTO Song (title, artistID, albumID, genreName, duration, audiolink, releaseDate)
+VALUES 
+    ('Default Mic', 2, 2, 'Rap', '00:01:29', 'https://firebasestorage.googleapis.com/v0/b/moseeqify.appspot.com/o/Mustafa%20G.M%20-%20default%20mic%20%5Bprod.%20HeyyLotus%5D%202023-04-29%2010_32.m4a?alt=media&token=1b4b8d49-56c0-4340-beae-e859f22c9e343', '2023-04-30')
+
+
+
+    playlistID INT PRIMARY KEY IDENTITY(1,1),
+    name VARCHAR(100) NOT NULL, -- Added NOT NULL constraint
+    creationdate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR(50) NOT NULL REFERENCES [User](username)
+
+INSERT INTO Playlist (NAME, username) VALUES ('Ammad Ki Playlist', 'nashanul')
+
+
+INSERT INTO PlaylistSongs (playlistID,songID) values 
+	(1,76),(1,77),(1,78),(1,79)
